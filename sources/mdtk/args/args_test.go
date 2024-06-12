3bdc09052f44001803b686561031efa930d842ad
m-YoC <67212285+m-YoC@users.noreply.github.com>
2024-06-12 22:18:14 +0900

    Fix args_test

--- sources/mdtk/args/args_test.go
+++ sources/mdtk/args/args_test.go
@@ -1,35 +1,33 @@
 package args
 
 import (
-	"fmt"
+	"mdtk/lib"
 	"testing"
 	"github.com/stretchr/testify/assert"
 )
 
+const (
+	positive = "positive"
+	negative = "negative"
+)
+
 func Test_Arg(t *testing.T) {
-	const (
-		positive = "positive"
-		negative = "negative"
-	)
-	
 	t.Run("Validate", func(t *testing.T) {
-		tests := []struct {
-			aname string
-			expected string
-		} {
-			{"key=value", positive},
-			{"_key=value", positive},
-			{"key=va lu e", positive},
-			{"key='va lu e'", positive},
-			{"key =value", negative},
-			{"ke~y=value", negative},
-			{"key: value", positive},
+		tests := lib.TestCases[string, string] {
+			{Name: "Alphabet=Alphabet is ok", Actual: "key=value", Expected: positive},
+			{Name: "Underbar is also ok", Actual: "_key=value", Expected: positive},
+			{Name: "In value, all chars are ok", Actual: "key=va &lu! e", Expected: positive},
+			{Name: "Can enclose value in quotes", Actual: "key='va lu e'", Expected: positive},
+			{Name: "Instead of '=', ':' is also ok", Actual: "key:value", Expected: positive},
+			{Name: "Between key and '=', have not to set space", Actual: "key =value", Expected: negative},
+			{Name: "Key's chars are only alphabet and underbar", Actual: "ke~y=value", Expected: negative},
+			{Name: "Key's chars are only alphabet and underbar", Actual: "ke y=value", Expected: negative},
 		}
 
 		for _, tt := range tests {
-			t.Run(fmt.Sprintf("'%s' is %v", tt.aname, tt.expected), func(t *testing.T) {
-				err := Arg(tt.aname).Validate()
-				if tt.expected == positive {
+			t.Run(tt.Name, func(t *testing.T) {
+				err := Arg(tt.Actual).Validate()
+				if tt.Expected == positive {
 					assert.NoError(t, err)
 				} else {
 					assert.Error(t, err)
@@ -39,23 +37,23 @@ func Test_Arg(t *testing.T) {
 	})
 
 	t.Run("GetData", func(t *testing.T) {
-		tests := []struct {
-			aname string
-			expected_k string
-			expected_v string
-			expected_err string
-		} {
-			{"key=value", "key", "value", positive},
+		type ExpectedT struct {
+			Key string
+			Value string
+			PN string
+		}
+		tests := lib.TestCases[string, ExpectedT] {
+			{Name: "Basic", Actual: "key=value", Expected: ExpectedT{Key: "key", Value: "value", PN: positive}},
+			{Name: "Key's chars are only alphabet and underbar", Actual: "ke y=value", Expected: ExpectedT{PN: negative}},
 		}
 
 		for _, tt := range tests {
-			t.Run(fmt.Sprintf("'%s' is %v and get key:%s, value:%s when positive", 
-					tt.aname, tt.expected_err, tt.expected_k, tt.expected_v), func(t *testing.T) {
-				k, v, err := Arg(tt.aname).GetData()
-				if tt.expected_err == positive {
+			t.Run(tt.Name, func(t *testing.T) {
+				k, v, err := Arg(tt.Actual).GetData()
+				if tt.Expected.PN == positive {
 					if assert.NoError(t, err) {
-						assert.Equal(t, tt.expected_k, k)
-						assert.Equal(t, tt.expected_v, v)
+						assert.Equal(t, tt.Expected.Key, k)
+						assert.Equal(t, tt.Expected.Value, v)
 					}
 				} else {
 					assert.Error(t, err)
@@ -68,26 +66,18 @@ func Test_Arg(t *testing.T) {
 }
 
 
-func Test_Args(t *testing.T) {
-	const (
-		positive = "positive"
-		negative = "negative"
-	)
-	
+func Test_Args(t *testing.T) {	
 	t.Run("Validate", func(t *testing.T) {
-		tests := []struct {
-			anames []string
-			expected string
-		} {
-			{[]string{"key=value", "key=value", "key=value"}, positive},
-			{[]string{"key=value", "key: value", "key=value"}, positive},
-			{[]string{"key=value", "key; value", "key=value"}, negative},
+		tests := lib.TestCases[[]string, string] {
+			{Name: "All is ok", Actual: []string{"key=value", "key=value", "key=value"}, Expected: positive},
+			{Name: "All is ok", Actual: []string{"key=value", "key: value", "key=value"}, Expected: positive},
+			{Name: "Includes ng", Actual: []string{"key=value", "key; value", "key=value"}, Expected: negative},
 		}
 
 		for _, tt := range tests {
-			t.Run(fmt.Sprintf("AND of '%v' is %v", tt.anames, tt.expected), func(t *testing.T) {
-				err := ToArgs(tt.anames...).Validate()
-				if tt.expected == positive {
+			t.Run(tt.Name, func(t *testing.T) {
+				err := ToArgs(tt.Actual...).Validate()
+				if tt.Expected == positive {
 					assert.NoError(t, err)
 				} else {
 					assert.Error(t, err)
--- sources/mdtk/lib/test_support.go
+++ sources/mdtk/lib/test_support.go
@@ -7,7 +7,7 @@ import (
 
 type TestCase[TA, TE any] struct {
 	Name string
-	Accepted TA
+	Actual TA
 	Expected TE
 }
 
@@ -16,12 +16,12 @@ type TestCases[TA, TE any] []TestCase[TA, TE]
 func Test_Example(t *testing.T) {
 	t.Run("desc", func(t *testing.T) {
 		tests := TestCases[string, string] {
-			{Name: "test", Accepted: "hello", Expected: "hello"},
+			{Name: "test", Actual: "hello", Expected: "hello"},
 		}
 		
 		for _, tt := range tests {
 			t.Run(tt.Name, func(t *testing.T) {
-				assert.Equal(t, tt.Expected, tt.Accepted)
+				assert.Equal(t, tt.Expected, tt.Actual)
 			})
 		}
 	})
