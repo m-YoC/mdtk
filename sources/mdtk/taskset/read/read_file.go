43d9241ccded2414ea178a917840faa453a0d7a7
m-YoC <67212285+m-YoC@users.noreply.github.com>
2024-07-06 10:51:05 +0900

    bugfix: Fixed ReadFile function not converting home directory '~'.

--- sources/mdtk/taskset/read/read_file.go
+++ sources/mdtk/taskset/read/read_file.go
@@ -8,7 +8,7 @@ import (
 )
 
 func ReadFile(path path.Path) Markdown {
-    b, err := os.ReadFile(path.FromSlash().String())
+    b, err := os.ReadFile(path.GetFileAbsPath().FromSlash().String())
     if err != nil {
         fmt.Println("File could not be read.")
         fmt.Println(path)
